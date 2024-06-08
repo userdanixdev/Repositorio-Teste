14da6c93f13867dc4fcd94a9177eebf28c5b4054
userdanixdev <f.daniel.m@gmail.com>
2024-06-08 15:00:10 -0300

    versão 04

---
+++ Pasta/Valores_aleatórios/Valores_aleatórios.vs04.py
@@ -0,0 +1,10 @@
+# Valores aleatórios: Versão 04
+# Crie uma programa que vai gerar 5 números aleatórios e colocar em uma tupla.
+# 1. Mostrar a listagem dos números gerados
+# 2. Mostrar o menor e o maior número que estão na tupla
+
+from random import sample  # importar da biblioteca random o método sample
+
+valores=(sample(range(10),5)) # Variável randômica
+valores_ordem = sorted(valores) 
+print(f'Os valores sorteados: {valores}\nO maior valor é: {valores_ordem[-1]}\nO menor valor é: {valores_ordem[0]}')
